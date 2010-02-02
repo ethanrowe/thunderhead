725ba876033438ac19cd44ad60cfe29e4cf34bcf
Ethan Rowe <ethan@endpoint.com>
2010-02-01 23:26:10 -0500

    GPLv3 license, copyright statements, etc.
    This project is now GPLed.  Delight floweth forth.

--- t/0004-account.py
+++ t/0004-account.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 
+##  Copyright (c) 2009-2010 Ethan Rowe (ethan@endpoint.com)
+##  For more information, see http://github.com/ethanrowe/thunderhead
+##
+##  This file is part of Thunderhead.
+##
+##  Thunderhead is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  Thunderhead is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with Thunderhead.  If not, see <http://www.gnu.org/licenses/>.
+
 import test_helper
 import thunderhead
 
